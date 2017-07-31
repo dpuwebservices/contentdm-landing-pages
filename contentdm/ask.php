74625b8a32071478098029b13259322f144095d3
Christine McClure <camcclure@gmail.com>
2017-07-31 14:12:02 -0500

    remove blank lines

--- contentdm/ask.php
+++ contentdm/ask.php
@@ -1,266 +1,39 @@
 <!DOCTYPE html><head>
-
-
-
-
-
 <title>Ask A Librarian</title>  
-
-
-
-
-
-
-
             <?php include 'header.php' ?>  
-
-
-
-
-
 <!-- Grid 1/2 -->  
-
-
-
-    
-
 <div class="wrapper">
-
-
-
-
-
-
-
 	<h1 class="pagetitle">Ask A Librarian</h1>
-
-
-
-
-
-
-
 		<div class="grid grid-pad">  
-
-
-
 			<div class="col-2-3">  
-
-
             	<!-- Grid 2/3 and 1/3-->  
-
-
-
                 <!-- Grid 1/3 -->  
-
-
-
             <div class="grid grid-pad">
-
-
-
-
-
-
-
 				<div class="col-1-1 ">
-
-
 <div class="col-1-1 recent-box-padding ">  	
-
-
-
-
-
-
-
-
-
-
-
-
-
                         <div class="col-1-2"><img class="centered" src="http://libservices.org/contentdm/images/ask/ByPhone.png"/>
-
-
-
-
-
-
-
-                        	
-
-
-
-
-
-
-
                         </div>
-
-
-
-
-
-
-
                         <div class="col-1-2"> 
-
-
-
-
-
-
-
-                        	
-
-
-
-
-
-
-
                              <div class="caption">
-
-
-
-
-
-
-
                            <h1>By Phone</h1>
                            <p>Lincoln Park (773) 325-7863<br>
                                Loop (312) 362-8432<br></p>
-                               
-
-
-
-
-
-
-
                              </div> 
-
-
-
-
-
-
-
-                            
-
-
-
-
-
-
-
                         </div>				
-
-
-
-
-
-
-
-            	
-
-
-
-
-
-
-
 </div>
-
-
-
-
-
-
-
                 <div class="col-1-1">  	
-
-
-
-
-
-
-
-                    	
-
-
-
-
-
-
-
                         <div class="col-1-2"><img class="centered" src="http://libservices.org/contentdm/images/ask/ByEmail.png"/></a>
-
-
-
-
-
-
-
-                        	
-
-
-
-
-
-
-
                         </div>
-
-
-
-
-
-
-
                         <div class="col-1-2"> 
                            <div class="caption">
                            <a href="#" onclick="window.open(	'http://depaul.libanswers.com/askr.php?i=680&amp;header=Email%20a%20Librarian&amp;uri=http%3A%2F%2Flibguides.depaul.edu%2Faecontent.php%3Fpid%3D386628%26sid%3D3172275%23','popUpWindow','height=700,width=800,left=10,top=10,resizable=yes,scrollbars=yes,toolbar=yes,menubar=no,location=no,directories=no,status=yes')">
-
                             <h1>By Email</h1></a>
-
                             <p>Ask a Librarian services are available to all students, faculty, staff and alumni of DePaul University. We will also answer questions from the general public that are directly related to the University or DePaul Libraries' collections and services.</p>
-
                              </div> 
-
                     </div>				
-
-
-
-
-
 				</div>
-
-
-
-
-
-
-
 <!--</body>
-
-
-
-
-
-
-
 </html>-->
-
-
-
-                        	
-
-
-
-
-
                              <div class="caption">
                                <div class="col-1-2"><img src="http://libservices.org/contentdm/images/ask/ByText.png" alt="" class="centered"/></a> </div>
                                <div class="col-1-2">
@@ -269,10 +42,6 @@
                                  </div>
                                </div>
                              </div>
-                    
-
-
-
                        <div class="caption">
                                <div class="col-1-2"><img src="http://libservices.org/contentdm/images/ask/Makeanappoinment.png" alt="" class="centered"/></a> </div>
                                <div class="col-1-2">
@@ -281,16 +50,6 @@
                                  </div>
                                </div>
                              </div>
-
-                        	
-
-
-
-
-
-                         
-                             
-   
                                  <div class="caption">
                                <div class="col-1-2"><img src="http://libservices.org/contentdm/images/ask/FAQ.png" alt="" class="centered"/> </div>
                                <div class="col-1-2">
@@ -299,161 +58,32 @@
                                  </div>
                                </div>
                              </div>
-                        
-
-
-                	
-
-
-
                 </div>
-
-
-
-				
-
-
-
                 </div>  
-
-
-
-                
-
-
-
                   	<div class="col-1-1">
-
-
-
                 <?php ?>
-
-
-
       				</div>
-
-
-
-				
-
-
-
-              	  
-
-
-
-
-
-
-
-	
-
-
-
 </div>
-
-
-
  <!-- end bento -->  
-
-
-
-				  
-
-
-
             <!-- about section-->  
-
-
-
 			<div class="col-1-3 border borderpad">  
-
-
-
 				<div class="content">  
-
-
-
                 		<div id='api_box_iid674_bid16224820'>
                         <div class="bi_text"></div>
                        <h2>IM a librarian</h2>
                        <p>&nbsp;</p>
-                      
-                      
-                   
 <a href="#" onclick="window.open('http://libraryh3lp.com/chat/dpulibrarian@chat.libraryh3lp.com?skin=22365&amp;theme=flat-chat-bubbles&amp;title=Ask%20a%20Librarian&amp;identity=dpulibrarian#','AskWindow', 'toolbar=no, directories=no,location=no, status=yes, menubar=no, resizable=no, scrollbars=yes, width=300,height=400'); return false" target="_blank" alt="Chat with a librarian"><img src="http://libguides.depaul.edu/loader.php?type=d&amp;id=940990" style="top: -1px; margin-top: -5px; display: block; margin-left: auto; margin-right: auto;" border="0" height="41px;" width="200px;"></a>                        
                         <p>Please note that texts and IM are best for brief, basic questions. Submit more complex, research strategy-type questions via the email link above or stop by any reference desk</p>
                         </div>
-
-				
-
-
-
-
-
-
-
-                        
-
-
-
-
-
-
-
                        <?php ?>  
-
-
-
          </div>     
-
-
-
 	</div>  
-
-
-
     </div>
-
-
-
-
-
-
-
 	</div>
-
-
-
-
-
-
-
 <div class="grid no-padding stick-bottom">  
-
-
-
 			<div class="col-1-1 ">  
-
-
-
             <?php include 'footer.php' ?>  
-
-
-
             </div>    
-
-
-
             </div> 	  
-
-
-
 </body>  
-
-
-
 </html>  
-
-
-
