488c1f909b9d8a1ef91c03e410f53c0484a64fbf
Christine McClure <camcclure@gmail.com>
2019-11-18 14:23:43 -0600

    check to see whether jquery loaded or not

--- responsive-site/scripts/customJS.js
+++ responsive-site/scripts/customJS.js
@@ -1,1 +1,10 @@
-alert("customjs file loaded");
+$(document).ready(function () {
+	if (jQuery) {
+		// jQuery is loaded  
+		alert("jQuery loaded!");
+	} else {
+		// jQuery is not loaded
+		alert("No JQuery");
+	}
+});
+
