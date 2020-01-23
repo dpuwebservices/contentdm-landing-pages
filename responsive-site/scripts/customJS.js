0687a32f244542b101bd57eb44d1eb2f464783f6
Christine McClure <camcclure@gmail.com>
2020-01-23 11:08:03 -0600

    added standard recipe from oclc site--also doesn't work

--- responsive-site/scripts/customJS.js
+++ responsive-site/scripts/customJS.js
@@ -35,3 +35,30 @@ function locationHashChanged() {
   });
 window.onhashchange = locationHashChanged;
 })();
+
+
+/* this function directly from the cookbook is also not working
+(function() {
+'use strict';
+
+  function urlRedirector() {
+    const baseUrl = window.location.origin
+      ? window.location.origin + '/'
+      : window.location.protocol + '//' + window.location.host + '/';
+    if (baseUrl == badUrl) {
+      window.location = goodUrl;
+    }
+  }
+
+  let badUrl = 'https://cdm15700.contentdm.oclc.org/';
+  let goodUrl = 'https://cdmdemo.contentdm.oclc.org';
+
+  // execute at earliest point in event lifecycle
+  document.addEventListener('cdm-app:ready', function() {
+    urlRedirector();
+  });
+
+})();
+
+/* version history
+*/
