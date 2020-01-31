730e26f4194b914d09fe3512d69bf457bfc1e282
Christine McClure <camcclure@gmail.com>
2020-01-31 13:34:17 -0600

    comment out all code while not working or approved

---
+++ responsive-site/scripts/change-search-box-text.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+//      
+//  function changeSearchBox() {
+//      var curPath=window.location.pathname; 
+//        var searchBox = document.getElementById('search-input');
+//
+//      if (curPath.endsWith('digital/')==true){
+//        searchBox.setAttribute("placeholder", "Search all collections");
+//      }
+//      else{
+//        searchBox.setAttribute("placeholder", "Search this collection");
+//      }
+//  }
+//
+//
+//  document.addEventListener('cdm-home-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-about-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-login-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-search-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-collection-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-advanced-search-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-item-page:ready', changeSearchBox);
+//  document.addEventListener('cdm-custom-page:ready', changeSearchBox);
+
+})();
--- responsive-site/scripts/customJS.js
+++ responsive-site/scripts/customJS.js
@@ -1,64 +1,38 @@
 'use strict';
 
-
-
-function checkPath(){
-  var curPath=window.location.pathname; 
-    var searchBox = document.getElementById('search-input');
-  
-  if (curPath.endsWith('digital/')==true){
-//    alert("on the home page");
-    searchBox.setAttribute("placeholder", "Search all collections");
-  }
-  else{
-    searchBox.setAttribute("placeholder", "Search this collection");
-  }
-}
-
-
-
-function locationHashChanged() {
-  if (location.hash === '#cool-feature') {
-    console.log("You're visiting a cool feature!");
-  }
-  else{
-    console.log("you're somewhere else");
-  }
-}
-
-
-(function() {
-  
-  // need to add listener for when page changes
-  document.addEventListener("DOMContentLoaded", function () {
-    checkPath();
-  });
-window.onhashchange = locationHashChanged;
-})();
-
-
-/* this function directly from the cookbook is also not working
-(function() {
-'use strict';
-
-  function urlRedirector() {
-    const baseUrl = window.location.origin
-      ? window.location.origin + '/'
-      : window.location.protocol + '//' + window.location.host + '/';
-    if (baseUrl == badUrl) {
-      window.location = goodUrl;
-    }
-  }
-
-  let badUrl = 'https://cdm15700.contentdm.oclc.org/';
-  let goodUrl = 'https://cdmdemo.contentdm.oclc.org';
-
-  // execute at earliest point in event lifecycle
-  document.addEventListener('cdm-app:ready', function() {
-    urlRedirector();
-  });
-
-})();
-
-/* version history
-*/
+//
+//
+//function checkPath(){
+//  var curPath=window.location.pathname; 
+//    var searchBox = document.getElementById('search-input');
+//  
+//  if (curPath.endsWith('digital/')==true){
+////    alert("on the home page");
+//    searchBox.setAttribute("placeholder", "Search all collections");
+//  }
+//  else{
+//    searchBox.setAttribute("placeholder", "Search this collection");
+//  }
+//}
+//
+//
+//
+//function locationHashChanged() {
+//  if (location.hash === '#cool-feature') {
+//    console.log("You're visiting a cool feature!");
+//  }
+//  else{
+//    console.log("you're somewhere else");
+//  }
+//}
+//
+//
+//(function() {
+//  
+//  // need to add listener for when page changes
+//  document.addEventListener("DOMContentLoaded", function () {
+//    checkPath();
+//  });
+//window.onhashchange = locationHashChanged;
+//})();
+//
