9dbe708085da83a707acd0737b908b3db51e36df
Christine McClure <camcclure@gmail.com>
2020-02-27 13:23:51 -0600

    changed logo. added script so clicking on logo goes to library url

--- responsive-site/scripts/customJS.js
+++ responsive-site/scripts/customJS.js
@@ -1,5 +1,17 @@
 (function() {
   'use strict';
+  
+    function changeLogoLink() {
+    const headerLogo = document.querySelector('div.Header-logoHolder>div>a');
+    if (headerLogo) {
+      headerLogo.href = newUrl;
+      headerLogo.addEventListener('click', function(e) {
+        window.open("https://library.depaul.edu/Pages/default.aspx", "_blank");
+        e.stopPropagation();
+      });
+    }
+  }
+  
 // Move the collection browse button to just underneath the title
   document.addEventListener('cdm-collection-landing-page:ready', function(e){
     var pageContainer = document.getElementsByClassName('CollectionLanding-maincontentLanding')[0];
@@ -8,5 +20,14 @@
 
     pageContainer.insertBefore(browseButton,pageTitle.nextSibling);
   });
+  
+  document.addEventListener('cdm-home-page:ready', changeLogoLink);
+  document.addEventListener('cdm-about-page:ready', changeLogoLink);
+  document.addEventListener('cdm-login-page:ready', changeLogoLink);
+  document.addEventListener('cdm-search-page:ready', changeLogoLink);
+  document.addEventListener('cdm-collection-page:ready', changeLogoLink);
+  document.addEventListener('cdm-advanced-search-page:ready', changeLogoLink);
+  document.addEventListener('cdm-item-page:ready', changeLogoLink);
+  document.addEventListener('cdm-custom-page:ready', changeLogoLink);
 
 })();
